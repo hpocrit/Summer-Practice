cd2ba283e95c52515131ec044ae4914648388add
Aliya <aliyagabdullina8@gmail.com>
2023-07-01 16:30:38 +0300

    Add homework №1

--- app/src/androidTest/java/com/example/summerpractice/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/summerpractice/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.commit
+package com.example.summerpractice
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.commit", appContext.packageName)
+        assertEquals("com.example.summerpractice", appContext.packageName)
     }
 }
