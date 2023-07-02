85e00a45b7098fe29c5135fa08b100d476115b29
Aliya <aliyagabdullina8@gmail.com>
2023-07-02 16:16:15 +0300

    Fixed homework #1

--- app/src/main/java/com/example/summerpractice/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import com.example.commit.R
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
